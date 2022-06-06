a7c3122440c1b8e930e3eaa4c1b9b4bdc234e907
byung ju choi <bjc30849@gmail.com>
2022-06-06 14:01:06 +0900

    chore: 중복 제거

--- utils/handleCheckListData.ts
+++ utils/handleCheckListData.ts
@@ -1,10 +1,9 @@
 import _ from 'lodash'
 import { IContent } from '../@types/checkList'
-import { findIndex } from 'lodash'
 
 export function getSliceCheckList(id: string, value: IContent[]) {
   //id기준으로 자름
-  const index = findIndex(value, { id: id })
+  const index = _.findIndex(value, { id: id })
   const front = value.slice(0, index)
   const back = value.slice(index + 1)
   return [[...front], [...back]]
